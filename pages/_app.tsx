51fa067142ab920ca9a96866bfd235c9f79aa29e
s5935512064 <32189965+s5935512064@users.noreply.github.com>
2022-12-19 14:14:24 +0700

    velaa_privillage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,57 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import type { Liff } from "@line/liff";
+import { useState, useEffect, use } from "react";
+import { useRouter } from "next/router";
+function MyApp({ Component, pageProps }: AppProps) {
+  const [liffObject, setLiffObject] = useState<Liff | null>(null);
+  const [liffError, setLiffError] = useState<string | null>(null);
+  const router = useRouter();
+  // Execute liff.init() when the app is initialized
+  useEffect(() => {
+    // to avoid `window is not defined` error
+    import("@line/liff")
+      .then((liff) => liff.default)
+      .then((liff) => {
+        console.log("LIFF init...");
+        liff
+          .init({ liffId: process.env.NEXT_PUBLIC_LIFF_ID! })
+          .then(() => {
+            console.log("LIFF init succeeded.");
+            // console.log(liff.isLoggedIn());
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+            if (!liff.isLoggedIn()) {
+              liff.login();
+            }
+
+            setLiffObject(liff);
+          })
+          .catch((error: Error) => {
+            console.log("LIFF init failed.");
+            setLiffError(error.toString());
+          });
+      });
+
+    // if (!liff.isLoggedIn()) {
+    //   liff.login();
+    // }
+  }, []);
+
+  useEffect(() => {
+    // liffObject.logout();
+    // if (!liffObject?.isLoggedIn()) {
+    //   console.log(liffObject?.isLoggedIn());
+    //   // liffObject?.login({
+    //   //   redirectUri: "https://www.google.com",
+    //   // });
+    // }
+  }, [liffObject]);
+
+  // Provide `liff` object and `liffError` object
+  // to page component as property
+  pageProps.liff = liffObject;
+  pageProps.liffError = liffError;
+  return <Component {...pageProps} />;
 }
+
+export default MyApp;
